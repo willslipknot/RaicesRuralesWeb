9b8382e6a54d8a9a7339064c6010c0f306bf5a19
Diego <willslipoknotpsico@gmail.com>
2023-09-26 21:49:47 -0500

    Cambios token y creacion de logout,profile

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import morgan from "morgan";
-import pool from './db.js';
 import authRoutes from './routes/auth.routes.js';
+import cookieParser from 'cookie-parser'
 
 
 const app = express();
 app.use(morgan('dev'));
 app.use(express.json()); 
+app.use(cookieParser());
 app.use(authRoutes);
+
 
 export default app;
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -34,7 +34,7 @@ export const register = async (req, res) => {
 
     console.log("Usuario creado");
     
-    const token= await createAccessToken({id:newUser._id});
+    const token= await createAccessToken({id:newUser.id});
     res.cookie('token', token);
     res.json({message:"Usuario creado correctamente"});
 
@@ -67,7 +67,7 @@ export const login = async (req, res) => {
       return res.status(401).json({ error: "Contraseña incorrecta." });
     }
 
-    const token= await createAccessToken({id:existingUser._id});
+    const token= await createAccessToken({id:existingUser.id});
     res.cookie('token', token);
     res.json({
         nombre: existingUser.nombre,
@@ -82,3 +82,32 @@ export const login = async (req, res) => {
   }
 };
 
+export const logout = async (req, res) =>{
+    res.cookie('token', "", {
+      expires:new Date(0)
+    })
+    return res.sendStatus(200)
+
+};
+
+export const profile = async (req, res) => {
+
+  try {
+    const userFound = await User.findByPk(req.user.id);
+
+    if (!userFound) {
+      return res.status(400).json({ message: "Usuario no encontrado" });
+    }
+
+    return res.json({
+      id: userFound.id,
+      nombre: userFound.nombre,
+      apellido: userFound.apellido,
+      correo: userFound.correo,
+      telefono: userFound.telefono
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Error interno del servidor" });
+  }
+};
